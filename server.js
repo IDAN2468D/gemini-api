5ce235d5503536dfe17acbed4a50e953a8ee7301
Idan Kazam <48420836+IDAN2468D@users.noreply.github.com>
2025-02-19 12:22:15 +0200

    Update

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const geminiRoutes = require("./src/routes/geminiRoutes");
+// const handleError  = require("./src/middleware/errorHandler");
 const authRoutes = require('./src/routes/authRoutes'); 
-const connectDB = require('./src/config/db.js'); // חיבור למסד נתונים
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || "0.0.0.0";
+const connectDB = require('./src/config/db.js');
 
-connectDB(); // ודא שחיבור מסד הנתונים לא נכשל
+connectDB();
 
 app.use(cors());
 app.use(express.json()); 
@@ -18,7 +20,7 @@ app.use(express.json());
 app.use("/api/gemini", geminiRoutes);
 app.use('/api/auth', authRoutes);
 
-// מאזין אך ורק לפורט
-app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on port ${PORT}`);
+
+app.listen(PORT, HOST, () => {
+    console.log(`Server running on http://${HOST}:${PORT}`);
 });
